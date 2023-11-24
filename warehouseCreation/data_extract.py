9b807f8f6df9d9064cc12c89d192cf74d025e121
Prasad213 <prasad2132018@outlook.com>
2023-11-24 18:39:51 +0530

    data_extractor class added

--- warehouseCreation/data_extract.py
+++ warehouseCreation/data_extract.py
@@ -1,3 +1,22 @@
+from sqlalchemy import inspect
 import pandas as pd
 
+class extractor():
+    def __init__(self,sqlalchemy_conn):
+        self.conn=sqlalchemy_conn
+    
+    def get_all_tables_name(self):
+        inspect(self.conn).get_table_names()
 
+    def get_all_tables_data(self):
+        table_names=self.get_all_tables_name()
+        [(table_name,pd.read_sql_table(table_name,self.conn)) for table_name in table_names]
+
+    def get_tables_data(self,table_name_list):
+        [(table_name,pd.read_sql_table(table_name,self.conn)) for table_name in table_name_list]
+
+    def get_table_dataframe(self,table_name:str):
+        pd.read_sql_table(table_name,self.conn)
+    
+    def query_to_dataframe(self,query:str):
+        pd.read_sql_query(query,self.conn)
