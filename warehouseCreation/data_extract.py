5646e991778f85056fc6109be812d4370571de36
Prasad213 <prasad2132018@outlook.com>
2023-12-01 21:14:14 +0530

    functions changed as per polars

--- warehouseCreation/data_extract.py
+++ warehouseCreation/data_extract.py
@@ -1,25 +1,18 @@
-from sqlalchemy import inspect
+from sqlalchemy import Connection
 import polars as pl
 
-class extraction():
-    def __init__(self,sqlalchemy_conn):
-        self.conn=sqlalchemy_conn
+def get_all_tables_name(sqlalchemy_conn:Connection):
+    from sqlalchemy import inspect
+    return inspect(sqlalchemy_conn).get_table_names()
+
+def get_all_tables_data(sqlalchemy_conn:Connection):
+    table_names=get_all_tables_name(sqlalchemy_conn)
+    query="select * from {}"
+    return [pl.read_database(query.format(table_name),sqlalchemy_conn) for table_name in table_names]
     
-    def get_all_tables_name(self):
-        inspect(self.conn).get_table_names()
-
-    def get_all_tables_data(self):
-        table_names=self.get_all_tables_name()
-        query="select * from {}"
-        return [(table_name, pl.read_database(query.format(table_name),self.conn)) for table_name in table_names]
-
-    def get_tables_data(self,table_name_list):
-        query="select * from {}"
-        return [(table_name, pl.read_database(query.format(table_name),self.conn)) for table_name in table_name_list]
-
-    def get_table_dataframe(self,table_name:str):
-        query="select * from {}"
-        return pl.read_database(query.format(table_name),self.conn)
+def get_table_dataframe(table_name:str,sqlalchemy_conn:Connection):
+    query="select * from {}"
+    return pl.read_database(query.format(table_name),sqlalchemy_conn)
     
-    def query_to_dataframe(self,query:str):
-        return pl.read_database(query),self.conn
+def query_to_dataframe(query:str,sqlalchemy_conn:Connection):
+    return pl.read_database(query,sqlalchemy_conn)
