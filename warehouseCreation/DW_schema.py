fe592158368f8d22b49cc9f10443e665e78a5cd3
Prasad213 <prasad2132018@outlook.com>
2023-12-03 21:15:38 +0530

    schema updated

--- warehouseCreation/DW_schema.py
+++ warehouseCreation/DW_schema.py
@@ -1,6 +1,6 @@
-olistdw_Customerdim_table = """
-IF object_id('Customerdim') is null
-CREATE TABLE Customerdim (
+customerdim = """
+IF object_id('customerdim') is null
+CREATE TABLE customerdim (
 customer_id VARCHAR(100) PRIMARY KEY,
 customer_unique_id VARCHAR (100) NOT NULL,
 customer_zip_code_prefix INT,
@@ -9,9 +9,9 @@ customer_state VARCHAR(50)
 );
 """
 
-olistdw_Productdim_table = """
-IF object_id('Productdim') is null
-CREATE TABLE Productdim (
+productdim = """
+IF object_id('productdim') is null
+CREATE TABLE productdim (
 product_id VARCHAR(100) PRIMARY KEY,
 product_category_name VARCHAR(200),
 product_photos_qty FLOAT(50),
@@ -22,9 +22,9 @@ product_width_cm FLOAT(50)
 );
 """
 
-olistdw_Sellerdim_table = """
-IF object_id('Sellerdim') is null
-CREATE TABLE Sellerdim (
+sellerdim = """
+IF object_id('sellerdim') is null
+CREATE TABLE sellerdim (
 seller_id VARCHAR(100) PRIMARY KEY,
 seller_zip_code_prefix INT,
 seller_city VARCHAR(50),
@@ -32,17 +32,17 @@ seller_state VARCHAR(50)
 );
 """
 
-olistdw_PaymentTypedim_table = """
-IF object_id('PaymentTypedim') is null
-CREATE TABLE PaymentTypedim (
+paymenttypedim = """
+IF object_id('paymenttypedim') is null
+CREATE TABLE paymenttypedim (
 payment_type_id INT PRIMARY KEY,
 payment_type VARCHAR(50) 
 );
 """
 
-olistdw_Datedim_table = """
-IF object_id('Datedim') is null
-CREATE TABLE Datedim (
+datedim = """
+IF object_id('datedim') is null
+CREATE TABLE datedim (
 DateKey INT NOT NULL PRIMARY KEY,
 FullDate DATETIME NOT NULL,
 Year INT NOT NULL,
@@ -54,11 +54,27 @@ DayOfWeek INT NOT NULL,
 DayName varchar(15) NOT NULL
 );
 """
-
-olistdw_OrderFact_table = """
-IF object_id('OrderFact') is null
-CREATE TABLE OrderFact (
+orderdatefact = """
+IF object_id('orderdatefact') is null
+CREATE TABLE orderdatefact (
 order_id VARCHAR(100) PRIMARY KEY,
+order_purchase_timestamp_key INT,
+order_approved_at_key INT,
+order_delivered_carrier_date_key INT,
+order_delivered_customer_date_key INT,
+order_estimated_delivery_date_key INT,
+FOREIGN KEY (order_purchase_timestamp_key) REFERENCES datedim(DateKey),
+FOREIGN KEY (order_approved_at_key) REFERENCES datedim(DateKey),
+FOREIGN KEY (order_delivered_carrier_date_key) REFERENCES datedim(DateKey), 
+FOREIGN KEY (order_delivered_customer_date_key) REFERENCES datedim(DateKey), 
+FOREIGN KEY (order_estimated_delivery_date_key) REFERENCES datedim(DateKey)
+);
+"""
+
+orderfact = """
+IF object_id('orderfact') is null
+CREATE TABLE orderfact (
+order_id VARCHAR(100),
 customer_id VARCHAR(100),
 seller_id VARCHAR(100),
 product_id VARCHAR(100),
@@ -69,7 +85,7 @@ payment_sequential INT,
 payment_installments INT,
 payment_value smallmoney,
 payment_type_id INT,
-FOREIGN KEY (order_id) REFERENCES OrderFact(order_id),
+FOREIGN KEY (order_id) REFERENCES orderdatefact(order_id),
 FOREIGN KEY (customer_id) REFERENCES Customerdim(customer_id),
 FOREIGN KEY (product_id) REFERENCES Productdim(product_id),
 FOREIGN KEY (seller_id) REFERENCES Sellerdim(seller_id),
@@ -77,25 +93,9 @@ FOREIGN KEY (payment_type_id) REFERENCES PaymentTypedim(payment_type_id)
 );
 """
 
-olistdw_OrderDateFact_table = """
-IF object_id('OrderDateFact') is null
-CREATE TABLE OrderDateFact (
-order_id VARCHAR(100) 
-order_purchase_timestamp_key INT,
-order_approved_at_key INT,
-order_delivered_carrier_date_key INT,
-order_delivered_customer_date_key INT,
-order_estimated_delivery_date_key INT,
-FOREIGN KEY (order_id)
-REFERENCES OrderFact(order_id)
-FOREIGN KEY (order_purchase_timestamp_key,order_approved_at_key,order_delivered_carrier_date_key, order_delivered_customer_date_key, order_estimated_delivery_date_key)
-REFERENCES OrderFact(DateKey)
-);
-"""
-
-olistdw_Reviewdim_table= """
-IF object_id('Reviewdim') is null
-CREATE TABLE Reviewdim (
+reviewdim= """
+IF object_id('reviewdim') is null
+CREATE TABLE reviewdim (
 order_id VARCHAR(100),
 review_score INT,
 review_comment_title VARCHAR(1000),
@@ -103,6 +103,6 @@ review_comment_message VARCHAR(8000),
 review_creation_date DATETIME,
 review_answer_timestamp DATETIME,
 FOREIGN KEY (order_id)
-REFERENCES OrderFact(order_id)
+REFERENCES orderdatefact(order_id)
 );
 """
